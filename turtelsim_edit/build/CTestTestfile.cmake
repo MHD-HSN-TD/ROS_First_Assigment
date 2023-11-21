c90656852eb69491df23100c0fae42d3665171f5
MHD-HSN-TD <mhdhasan.alkhalil@gmail.com>
2023-11-21 19:05:00 +0200

    add turtulesim_edit

---
+++ turtelsim_edit/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hsn/catkin_wc/src/turtelsim_edit
+# Build directory: /home/hsn/catkin_wc/src/turtelsim_edit/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
